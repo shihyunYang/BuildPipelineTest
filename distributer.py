4e6c0c2633785c732deebbb2de9eedd6c10776b8
shihyunYang <48581995+shihyunYang@users.noreply.github.com>
2023-03-17 19:14:56 +0900

    tnwjd

--- distributer.py
+++ distributer.py
@@ -2,7 +2,7 @@
 import shutil
 
 def greet():
-    shutil.copy2('F:/tttt.mp4', 'F:/asdf/tttt.mp4')
+    shutil.copy2('../Builds/BuildPlayer.apk', '/Users/yangsihyeon/Sites/build/BuildPlayer.apk')
 
 if __name__ == "__main__":
     greet()
