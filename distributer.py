17762600a69cc4e36431403adfcb3cffaa5dde81
shihyunYang <48581995+shihyunYang@users.noreply.github.com>
2023-03-20 11:34:49 +0900

    dd

--- distributer.py
+++ distributer.py
@@ -2,7 +2,7 @@
 import shutil
 
 def greet():
-    shutil.copy2('../Builds/BuildPlayer.apk', '/Users/yangsihyeon/Sites/build/BuildPlayer.apk')
+    shutil.copy2('/Users/yangsihyeon/Desktop/BuildPipelineTest/Builds/BuildPlayer.apk', '/Users/yangsihyeon/Sites/build/BuildPlayer.apk')
 
 if __name__ == "__main__":
     greet()
