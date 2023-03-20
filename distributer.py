c15c443d61367b379d46c5c2d1a5769bbda0a171
shihyunYang <48581995+shihyunYang@users.noreply.github.com>
2023-03-20 15:04:51 +0900

    253
    3

--- distributer.py
+++ distributer.py
@@ -17,8 +17,7 @@ def webpageCreate(strname):
                 <p><a href="build/{0}">install {0} dev-aos</a></p>
             </body>
         </html>
-        """
-    webpage.format(strname)
+        """.format(strname)
     
     
     file.write(webpage)
@@ -27,12 +26,10 @@ def webpageCreate(strname):
 
 def greet():
     x = dt.datetime.now()
-    test = '/Users/yangsihyeon/Sites/build/BuildPlayer{0}_{1}_{2}_{3}_{4}_{5}_[6].apk'
-    test.format(x.year, x.month, x.day, x.hour, x.minute, x.second)
+    test = '/Users/yangsihyeon/Sites/build/BuildPlayer{0}_{1}_{2}_{3}_{4}_{5}_[6].apk'.format(x.year, x.month, x.day, x.hour, x.minute, x.second)
     shutil.copy2('/Users/yangsihyeon/Desktop/BuildPipelineTest/Builds/BuildPlayer.apk', test)
     
-    filename = 'BuildPlayer{0}_{1}_{2}_{3}_{4}_{5}_[6].apk'
-    filename.format(x.year, x.month, x.day, x.hour, x.minute, x.second)
+    filename = 'BuildPlayer{0}_{1}_{2}_{3}_{4}_{5}_[6].apk'.format(x.year, x.month, x.day, x.hour, x.minute, x.second)
     webpageCreate(filename)
     
 
